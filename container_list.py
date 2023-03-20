dfbf29b9abd93ba9f168af148aa2ef901394811f
Rik <97382691+sontaimnt@users.noreply.github.com>
2023-03-20 09:10:59 +0530

    Create container_list.py

---
+++ container_list.py
@@ -0,0 +1,14 @@
+alpineimg = ["alpine:3.15" , "alpine:3.16" , "alpine:latest"]
+archimg = ["archlinux:latest"]
+debimg = ["debian:9" , "debian:10" , "debian:stable" , "debian:testing" , "debian:unstable"]
+fedimg = ["fedora:36" , "fedora:37" , "fedora:38"]
+oslimg = ["registry.opensuse.org/opensuse/leap:latest"]
+ostimg = ["registry.opensuse.org/opensuse/tumbleweed:latest"]
+ubuimg = ["ubuntu:14.04" , "ubuntu:16.04" , "ubuntu:18.04" , "ubuntu:20.04" , "ubuntu:22.04" , "ubuntu:latest"]
+
+
+alpineid = ["[blue]Alpine Linux 3.15[/blue]" , "[blue]Alpine Linux 3.16[/blue]" , "[blue]Alpine Linux Latest[/blue]"]
+archid = ["[cyan]Arch Linux Latest[/cyan]"]
+debid = ["[red]Debian 9(Stretch)[/red]" , "[red]Debian 10(Buster)[/red]" , "[red]Debian 11(Bullseye)[/red]" , "[red]Debian Testing[/red]" , "[red]Debian Sid[/red]"]
+fedid = ["[blue]Fedora 36[/blue]" , "[blue]Fedora 37[/blue]" , "[blue]Fedora 38[/blue]"]
+opensusid = ["[green]Open Suse Leap[/green]" , "[green]Open Suse Tumbleweed[/green]"]
